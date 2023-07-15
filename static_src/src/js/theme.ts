b3ae5c10bbf99c0ee968e4cdd7534935c490236c
Jamie Murphy <hello@itsjamie.dev>
2023-07-15 15:06:20 -0700

    index: Add Usage box

--- static_src/src/js/theme.ts
+++ static_src/src/js/theme.ts
@@ -5,37 +5,90 @@ const button = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
 const MastodonShareButton = document.getElementById("mastodon_share_button");
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {
+	const CopyToClipboardButton = document.getElementById("copy_clipboard_btn");
+	const UsageBox = document.getElementById("usage_box");
+
+	// Lock the text copy function
+	var hasCopiedText = false;
+	CopyToClipboardButton?.addEventListener("click", async (event) => {
+		if (!hasCopiedText) {
+			hasCopiedText = true;
+
+			let original_child = CopyToClipboardButton.innerHTML;
+			let usage_text = UsageBox?.firstChild?.textContent;
+
+			if (usage_text) {
+				await navigator.clipboard.writeText(usage_text)
+
+				CopyToClipboardButton.innerHTML = "Copied!";
+			}
+
+			setTimeout(() => {
+				CopyToClipboardButton.innerHTML = original_child;
+				hasCopiedText = false;
+			}, 3000);
+		}
+
+		event.stopPropagation();
+	});
+
 	const copyright = document.getElementById("copyright");
 	if (copyright != null) {
 		const year = new Date().getFullYear();
 		copyright.innerText = `${year} Luna Dragon`;
 	}
-});
 
-function ShareOnMastodon(): void {
-	const contentelm = document.getElementById(
-		"content",
-	) as HTMLTextAreaElement | null;
-	const InstanceUrlform = document.getElementById(
-		"instance_url",
-	) as HTMLInputElement | null;
-	if (contentelm != null && InstanceUrlform != null) {
-		const content = encodeURIComponent(contentelm.value);
+	if (button != null) {
+		button.addEventListener("click", () => {
+			if (menu != null) {
+				menu.classList.toggle("hidden");
+			}
+		});
+	}
 
-		let url = InstanceUrlform.value;
-		if (url === "") {
-			url = "https://hachyderm.io";
+	if (toggle != null) {
+		if (
+			localStorage.theme === "light" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: light)").matches)
+		) {
+			document.documentElement.classList.remove("dark");
+			toggle.innerHTML =
+				'<i id="darkmode_icon" class="fa-solid fa-moon" aria-label="Switch between dark and light mode" aria-hidden="true"></i>';
+			if (ThemeRailcasts != null && ThemeTomorrow != null) {
+				console.log("theme switch");
+				ThemeTomorrow.removeAttribute("disabled");
+				ThemeRailcasts.setAttribute("disabled", "disabled");
+			}
+		} else {
+			document.documentElement.classList.add("dark");
+			toggle.innerHTML =
+				'<i id="darkmode_icon" class="fa-solid fa-sun" aria-label="Switch between dark and light mode" aria-hidden="true"></i>';
+			if (ThemeRailcasts != null && ThemeTomorrow != null) {
+				ThemeRailcasts.removeAttribute("disabled");
+				ThemeTomorrow.setAttribute("disabled", "disabled");
+			}
 		}
 
-		const masto_url = `${url}/share?text=${content}`;
-
-		window.open(masto_url, "_blank");
+		toggle.addEventListener("click", () => {
+			console.log("click");
+			const icon = document.getElementById("darkmode_icon");
+			if (icon != null) {
+				if (
+					icon.classList.contains("fa-moon") ||
+					localStorage.getItem("theme") === "light"
+				) {
+					setTheme("dark");
+				} else if (
+					icon.classList.contains("fa-sun") ||
+					localStorage.getItem("theme") === "dark"
+				) {
+					setTheme("light");
+				}
+			}
+		});
 	}
-}
-
-MastodonShareButton?.addEventListener("click", () => {
-	ShareOnMastodon();
 });
 
 function setTheme(mode: string): void {
@@ -61,55 +114,4 @@ function setTheme(mode: string): void {
 		}
 	}
 	localStorage.setItem("theme", mode);
-}
-
-if (button != null) {
-	button.addEventListener("click", () => {
-		if (menu != null) {
-			menu.classList.toggle("hidden");
-		}
-	});
-}
-
-if (toggle != null) {
-	if (
-		localStorage.theme === "light" ||
-		(!("theme" in localStorage) &&
-			window.matchMedia("(prefers-color-scheme: light)").matches)
-	) {
-		document.documentElement.classList.remove("dark");
-		toggle.innerHTML =
-			'<i id="darkmode_icon" class="fa-solid fa-moon" aria-label="Switch between dark and light mode" aria-hidden="true"></i>';
-		if (ThemeRailcasts != null && ThemeTomorrow != null) {
-			console.log("theme switch");
-			ThemeTomorrow.removeAttribute("disabled");
-			ThemeRailcasts.setAttribute("disabled", "disabled");
-		}
-	} else {
-		document.documentElement.classList.add("dark");
-		toggle.innerHTML =
-			'<i id="darkmode_icon" class="fa-solid fa-sun" aria-label="Switch between dark and light mode" aria-hidden="true"></i>';
-		if (ThemeRailcasts != null && ThemeTomorrow != null) {
-			ThemeRailcasts.removeAttribute("disabled");
-			ThemeTomorrow.setAttribute("disabled", "disabled");
-		}
-	}
-
-	toggle.addEventListener("click", () => {
-		console.log("click");
-		const icon = document.getElementById("darkmode_icon");
-		if (icon != null) {
-			if (
-				icon.classList.contains("fa-moon") ||
-				localStorage.getItem("theme") === "light"
-			) {
-				setTheme("dark");
-			} else if (
-				icon.classList.contains("fa-sun") ||
-				localStorage.getItem("theme") === "dark"
-			) {
-				setTheme("light");
-			}
-		}
-	});
 }
