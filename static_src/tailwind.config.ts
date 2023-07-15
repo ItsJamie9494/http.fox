26ccf17fe3d1234ea723493e684fbe3b1438f3a7
Jamie Murphy <hello@itsjamie.dev>
2023-07-15 16:08:50 -0700

    static: Adjust yarn config

--- static_src/tailwind.config.ts
+++ static_src/tailwind.config.ts
@@ -4,5 +4,4 @@ import { Config } from "tailwindcss";
 module.exports = {
 	darkMode: "class",
 	content: ["../templates/*.html.tera", "../static/**/*.js"],
-	plugins: [require("@tailwindcss/forms")],
 } as Config;
