2f76d551ac7b8578fc23931cb8ea864b66febc76
Jamie Murphy <hello@itsjamie.dev>
2023-07-15 13:08:02 -0700

    build: Add a build script for generating static files

---
+++ build.rs
@@ -0,0 +1,20 @@
+use std::{env, error::Error, process::Command};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=static_src");
+    println!("cargo:rerun-if-changed=static");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    env::set_current_dir("static_src")?;
+
+    Command::new("yarn").args(&["install"]).output()?;
+    Command::new("yarn").args(&["check"]).output()?;
+
+    if Ok("release".to_owned()) == env::var("PROFILE") {
+        Command::new("yarn").args(&["compile:prod"]).output()?;
+    } else {
+        Command::new("yarn").args(&["compile"]).output()?;
+    }
+
+    Ok(())
+}
