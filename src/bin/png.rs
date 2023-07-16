dead2187ec8e10610ec119e785e67738ac416f12
Jamie Murphy <hello@itsjamie.dev>
2023-07-15 22:49:34 -0700

    png: Add support for generating one image at a time

--- src/bin/png.rs
+++ src/bin/png.rs
@@ -3,8 +3,36 @@ use std::str::FromStr;
 
 use httpfox::{config::Config, png::Png};
 
+fn make_image(config: &Config, file_name: &str) -> Result<(), Box<dyn Error>> {
+    println!("Converting image {file_name}");
+
+    let status_code = i32::from_str(&file_name.replace("_raw", ""))?;
+
+    match Png::new(&config, status_code) {
+        Some(png) => {
+            png.image();
+            println!("Successfully converted image {file_name}");
+        }
+        None => {
+            println!("Image {file_name} is not a valid status image, skipping")
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::default();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let code = &args[1];
+
+        make_image(&config, code)?;
+
+        return Ok(());
+    }
 
     for f in config.raw_images_dir.read_dir()? {
         let file = f?;
@@ -17,19 +45,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .ok_or(Box::<dyn Error>::from("Could not get File Name"))?;
 
         if file_name.contains("raw") && file.path().extension().is_some_and(|ext| ext == "png") {
-            println!("Converting image {file_name}");
-
-            let status_code = i32::from_str(&file_name.replace("_raw", ""))?;
-
-            match Png::new(&config, status_code) {
-                Some(png) => {
-                    png.image();
-                    println!("Successfully converted image {file_name}");
-                }
-                None => {
-                    println!("Image {file_name} is not a valid status image, skipping")
-                }
-            }
+            make_image(&config, file_name)?;
         }
     }
 
